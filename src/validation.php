0da5490606669b59ac574b5580549c92f65b5b47
Marco Bramato <marco@bramato.com>
2022-01-13 14:30:36 +0100

    Fix preg_match

--- src/validation.php
+++ src/validation.php
@@ -99,7 +99,7 @@ function isInteger($value, $unsigned = true, $acceptIntegerFloatingPoints = fals
     }
 
     //accept only integer number and if $acceptIntegerFloatingPoints is true accept integer floating point too.
-    return ((preg_match('/^' . ($unsigned ? '' : '-{0,1}') . '[0-9]{1,}$/', $value) === 1
+    return ((preg_match('/^' . ($unsigned ? '' : '-{0,1}') . '[0-9]{1,}$/', (int) $value) === 1
             && ($value <= PHP_INT_MAX && $value >= PHP_INT_MIN && (((int)$value) == $value))
         )
         || ($acceptIntegerFloatingPoints && isIntegerFloatingPoint($value, $unsigned)));
